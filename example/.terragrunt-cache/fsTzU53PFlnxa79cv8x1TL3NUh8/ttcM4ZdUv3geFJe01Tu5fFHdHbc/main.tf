5aa284a3bc078487c4132831422dc34d690b46e3
David Rivera <David.Rivera@ucop.edu>
2020-10-19 07:42:51 -0700

    update of flow logs - rework

--- example/.terragrunt-cache/fsTzU53PFlnxa79cv8x1TL3NUh8/ttcM4ZdUv3geFJe01Tu5fFHdHbc/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_flow_log" "logs" {
-  count         = var.enabled ? length(var.vpc_ids) : 0
-  log_destination      = var.log_destination
-  log_destination_type = "s3"
-  traffic_type         = "ALL"
-  vpc_id               = var.vpc_ids[count.index]
-}
